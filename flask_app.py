ea5bfb9fe01b7dab1e3eff59d1861e67c9bce7bb
anfy333 <99250133+anfy333@users.noreply.github.com>
2022-11-19 09:27:01 +0000

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,24 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+import hashlib
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def signup():
     return render_template("signup.html")
 
-@app.route("/home", methods=['POST'])
-def signup():
+@app.route("/signup", methods=['POST'])
+def sign():
 	password = request.form['pw']
 	en = password.encode()
 	h = hashlib.sha256(en)
 	password = h.hexdigest()
 	con = sqlite3.connect('login.db')
 	cur = con.cursor()
-	cur.execute("""INSERT INTO Users (Username, Password) VALUES (?,?)""",
-		(request.form['un'], password))
+	cur.execute("""INSERT INTO Users (Username, Password, FirstName, LastName, Email) VALUES (?,?,?,?,?)""",
+		(request.form['un'], password, request.form['fn'], request.form['ln'], request.form['em']))
 	con.commit()
-	return 'welcome '+ request.form['un']
+	user = request.form['un']
+	return redirect(url_for('home', name = user))
 
 @app.route("/login")
 def login():
@@ -26,15 +28,25 @@ def login():
 
 @app.route('/verify', methods=['POST'])
 def verify():
+	password = request.form['pw']
+	en = password.encode()
+	h = hashlib.sha256(en)
+	password = h.hexdigest()
 	con = sqlite3.connect('login.db')
 	cur = con.cursor()
 	cur.execute("SELECT * FROM Users WHERE Username=? AND Password=?",
-		(request.form['un'],request.form['pw']))
+		(request.form['un'],password))
 	match = len(cur.fetchall())
+	user = request.form['un']
 	if match == 0:
-		return "Wrong username and password"
+		return "Wrong username or password"
 	else:
-		return "Welcome " + request.form['un']
+		return redirect(url_for('home', name = user))
+
+@app.route("/home/<name>")
+def home(name):
+	#text = request.get_json().get("message")
+	return render_template("home.html", name = name)
 
 @app.route("/db")
 def db():
@@ -49,6 +61,7 @@ def db():
     except sqlite3.OperationalError as e:
         return str(e)
     return "table created"
+
 @app.route('/insert')
 def insert():
 	con = sqlite3.connect('login.db')
@@ -58,6 +71,7 @@ def insert():
 			""")
 	con.commit()
 	return 'INSERT'
+
 @app.route('/select')
 def select():
 	con = sqlite3.connect('login.db')
@@ -65,3 +79,13 @@ def select():
 	cur.execute("SELECT * FROM Users")
 	rows = cur.fetchall()
 	return str(rows)
+
+@app.route('/alter')
+def alter():
+	con = sqlite3.connect('login.db')
+	cur = con.cursor()
+	cur.execute(	"""	ALTER TABLE Users
+					ADD Email VARCHAR(50)
+			""")
+	con.commit()
+	return 'alter'
